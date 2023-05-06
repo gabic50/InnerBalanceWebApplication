6d56966fceae45749fb6c67ed3cc77df7e884b8f
“gabic50” <gcoope201@caledonian.ac.uk>
2023-05-06 03:05:17 +0100

    Add comments to Routes

--- routes/wellnessRoutes.js
+++ routes/wellnessRoutes.js
@@ -1,3 +1,4 @@
+//The routes set which page the user will view within the application
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/wellnessControllers.js");
@@ -8,6 +9,7 @@ router.get("/achievements", controller.landing_page_achievements);
 //router.get("/login", controller.landing_page_login);
 //router.get("/register", controller.landing_page_register);
 
+//When the application is first open it will go to the home page
 router.get("/", function (req, res) {
   res.redirect("/homepage.html");
 });
@@ -26,15 +28,12 @@ router.get("/register", function (req, res) {
 
 router.get("/newFitness", controller.new_entries_fitness);
 router.post("/newFitness", controller.post_new_entry_fitness);
-//router.get("/login", controller.login);
-//router.post("/login", controller.post_login);
-//router.get("/nutrition", controller.show_entries_nutrition);
 router.get("/newNutrition", controller.new_entries_nutrition);
 router.post("/newNutrition", controller.post_new_entry_nutrition);
-//router.get("/nutrition", controller.delete_entry); // Route for deleting entries
 router.get("/newLifestyle", controller.new_entries_lifestyle);
 router.post("/newLifestyle", controller.post_new_entry_lifestyle);
 
+//Error handling
 router.use(function (req, res) {
   res.status(404);
   res.type("text/plain");
