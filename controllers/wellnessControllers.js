a4e724d0e045357907f4b725548c204179e52d49
“gabic50” <gcoope201@caledonian.ac.uk>
2023-05-06 02:52:43 +0100

    Add comments to Controller

--- controllers/wellnessControllers.js
+++ controllers/wellnessControllers.js
@@ -1,13 +1,17 @@
+//Creates the Database that will store the goals of the user
 const wellnessDAO = require("../models/wellnessModel");
 const db = new wellnessDAO();
 
+//This will display the goals that have been added to give the user an idea of what goals to create
 db.init();
 
+//This will display if no gaols have been set by the user
 exports.entries_list = function (req, res) {
   res.send("<h1>No Goals set.</h1>");
   db.getAllEntries();
 };
 
+//This will display the achievements page
 exports.landing_page_achievements = function (req, res) {
   db.getAllCompletedEntries()
     .then((list) => {
@@ -22,6 +26,7 @@ exports.landing_page_achievements = function (req, res) {
     });
 };
 
+//This will display the fitness page
 exports.landing_page_fitness = function (req, res) {
   db.getAllFitnessEntries()
     .then((list) => {
@@ -36,6 +41,7 @@ exports.landing_page_fitness = function (req, res) {
     });
 };
 
+//This will display the nutrition page
 exports.landing_page_nutrition = function (req, res) {
   db.getAllNutritionEntries()
     .then((list) => {
@@ -50,6 +56,7 @@ exports.landing_page_nutrition = function (req, res) {
     });
 };
 
+//This will display the lifestyle page
 exports.landing_page_lifestyle = function (req, res) {
   db.getAllLifestyleEntries()
     .then((list) => {
@@ -63,12 +70,15 @@ exports.landing_page_lifestyle = function (req, res) {
       console.log("promise rejected", err);
     });
 };
+
+//Take user to the create a new fitness goal
 exports.new_entries_fitness = function (req, res) {
   res.render("newEntry", {
     title: "Set Fitness Goal",
   });
 };
 
+//Posts the user new fitness goal
 exports.post_new_entry_fitness = function (req, res) {
   if (!req.body.goalTitle) {
     response.status(400).send("Entries must have an title.");
@@ -85,12 +95,14 @@ exports.post_new_entry_fitness = function (req, res) {
   res.redirect("/fitness");
 };
 
+//Take user to the create a new nutrition goal
 exports.new_entries_nutrition = function (req, res) {
   res.render("newEntry", {
     title: "Set Nutrition Goal",
   });
 };
 
+//Posts the user new nutrition goal
 exports.post_new_entry_nutrition = function (req, res) {
   console.log("processing post-new_entry controller");
   if (!req.body.goalTitle) {
@@ -108,19 +120,14 @@ exports.post_new_entry_nutrition = function (req, res) {
   res.redirect("/nutrition");
 };
 
-exports.delete_entry = function (req, res) {
-  var goalTitle = req.params.goalTitle;
-  console.log("processing delete_entry controller: ", goalTitle);
-  db.deleteEntry(goalTitle);
-  res.redirect("/goals");
-};
-
+//Take user to the create a new lifestyle goal
 exports.new_entries_lifestyle = function (req, res) {
   res.render("newEntry", {
     title: "Set Lifestyle Goal",
   });
 };
 
+//Posts the user new lifestyle goal
 exports.post_new_entry_lifestyle = function (req, res) {
   console.log("processing post-new_entry controller");
   if (!req.body.goalTitle) {
@@ -136,16 +143,4 @@ exports.post_new_entry_lifestyle = function (req, res) {
     req.body.complete
   );
   res.redirect("/lifestyle");
-};
-
-exports.post_delete_goal = function (req, res) {
-  console.log("processing post-delete_goal controller");
-  if (!req.query.goalTitle) {
-    console.log("IN IF STATEMENT");
-    res.status(400).send("Entries must have an Goal Name.");
-    return;
-  }
-  console.log("BEFORE DB ENTRY");
-  db.deleteGoal(req.query.goalTitle);
-  res.redirect("/fitness");
 };
